5607831ffaebafe1c2007674698c9b3c47f21207
Brian Carlton <brian.carlton@ou.edu>
2021-11-05 15:37:30 -0500

    Added Documentation

--- julia/pcfm.jl
+++ julia/pcfm.jl
@@ -7,6 +7,19 @@ plot()
 function autocorr(x)
   return conv(x,x[end:-1:1])
 end
+"""
+    ∇J(B,Bb,x,m,u,a)
+Compute the gradient for the PCFM algorithm.
+  ...
+# Arguments
+- `B::Array`: M X M Orthonormal basis.
+- `Bb::Array`: 2M-1 X M Orthonormal basis.
+- `x::Vector`: M X 1 Phase coefficients.
+- `m::Integer`: Size.
+- `u::Vector`: 2M-1 X 1 Window Function.
+- `a::Integer`: Log base. If zero than use non-log version.
+...
+"""
 function ∇J(B,Bb,x,m,u,a)
   s = exp.(im.*B*x)
   sb = vcat(s, zeros(m-1,1))
@@ -14,22 +27,34 @@ function ∇J(B,Bb,x,m,u,a)
   sbf = sbf ./maximum(abs.(sbf))
   if(a == 0)
     diff = abs.(sbf).^2 .-u
-    J = norm(diff,2)
-    return (J,2/J .*transpose(Bb)*imag.(conj.(sb).*ifft(ifftshift(diff.*sbf))))
+    factor = 2
   else
     diff = log.(a,abs.(sbf).^2) .-log.(a,u)
-    J = norm(diff,2)
-    return (J,2/(log(a)*J) .*transpose(Bb)*imag.(conj.(sb).*ifft(ifftshift(diff.*sbf))))
+    factor = 2/(log(a))
   end
+  J = norm(diff,2)
+  #Need shift since PSD is centered about 0.
+  return (J,factor/(J).*transpose(Bb)*imag.(conj.(sb).*ifft(ifftshift(diff.*sbf))))
 end
+"""
+  funPcfmHelper(m,K)
+Computes extra values for use in gradient calculation.
+  ...
+# Arguments
+- `m::Integer`: Size.
+- `K::Integer`: Oversampling Factor.
+...
+"""
 function funPcfmHelper(m,K)
   nt = m
   minAlpha = -pi/K
   maxAlpha = pi/K
+  #Hard way of making a ramp function.
   g = ones(K, 1)./K
   g = vcat(g, zeros(m-K,1))
   B = cumsum(g, dims = 1)
   for i = 2:nt
+    #Logical Shift g
     g = vcat(0, g[1:end-1])
     B = hcat(B, cumsum(g, dims = 1))
   end
@@ -37,22 +62,36 @@ function funPcfmHelper(m,K)
   Bb = vcat(B,zeros(m-1, nt))
   return (B,Bb,x)
 end
+"""
+  funPcfm(u,a,iter,K)
+PCFM algorithm. Returns a randomly initialized vector with PSD of u.
+  ...
+# Arguments
+- `u::Vector`: Window Function PSD.
+- `a::Integer`: Log base. If zero do non log version.
+- `iter::Integer`: Number of iterations.
+- `K::Integer`: Oversampling Factor.
+...
+"""
 function funPcfm(u,a,iter,K)
-  m::Int64 = trunc((length(u)+1)/2)
+  #Calculate m from u.
+  m = trunc(Int,(length(u)+1)/2)
   (B,Bb,x) = funPcfmHelper(m,K)
-  epsilon = 10^-2
+  #Gradient Descent Parameters
   μ = 0.25
   β = 0.5
+  #Vector of error at each iteration.
   Jvec = ones(iter-1,1)
   i = 1
   vtOld = 0;
   while i <= iter
+    #Nesterov Accelerated Descent
     (J,∇)=∇J(B,Bb,x.-β.*vtOld,m,u,a)
     Jvec[i:end] .= J
     vt = β.*vtOld.+μ.*∇
     x -= vt
     vtOld = vt
-
+    #Extra Functions fro visulation.
     #s = exp.(im.*B*x)
     #sb = vcat(s, zeros(m-1,1))
     #sbf =  fftshift(fft(sb))
--- julia/pcfmDriver.jl
+++ julia/pcfmDriver.jl
@@ -1,18 +1,33 @@
 using CSV
 using DelimitedFiles
+include("pcfm.jl")
+"""
+    exportAndDisplay(u,a,iter,K,m,name)
+Exports and displays PCFM algorith results.
+  ...
+# Arguments
+- `u::Vector`: Window Function PSD.
+- `a::Integer`: Log base. If zero use non log version.
+- `iter::Integer`: Number of iterations.
+- `K::Integer`: Oversampling factor.
+- `m::Integer`: Size.
+- `name::String`: Name of window function.
+...
+"""
 function exportAndDisplay(u,a,iter,K,m,name)
     u[findall(<(-50), 10*log10.(u))] .= 10^-5
     u = abs.(u).^2
     result = funPcfm(u,a,iter,K)
     (B,Bb,x) = funPcfmHelper(m,K)
     s = exp.(im.*B*result)
-    writedlm( name*".csv",  s, ',')
+    writedlm( name*".csv",  [real(s),imag(s)], ',')
     sb = vcat(s, zeros(m-1,1))
     sbf =  fftshift(fft(sb))
     sbf = sbf ./maximum(abs.(sbf))
     display(plot(10*log10.(abs.(sbf).^2),ylim=(-50, 0)))
     display(plot!(10*log10.(u),ylim=(-50, 0)))
 end
+
 m = 128
 K=3
 a = 10
@@ -35,4 +50,3 @@ exportAndDisplay(u,a,iter,K,m,"Rectangular")
 #Triangular Window
 u = triang((2*m-1,1); padding = 0, zerophase = false)
 exportAndDisplay(u,a,iter,K,m,"Triangular")
-
